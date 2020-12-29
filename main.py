3030c969bda88886d80a96228193e190cc3337d6
GregCocks-Personal <skiwi@spamcop.net>
2020-12-29 00:59:21 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn import preprocessing
+
+print('... modules, etc imported')
